8b33ee92fb686619b35b4cb4bc9880c7a5007f58
Eugene Homyakov <ehomyakov@gmail.com>
2014-11-17 15:51:20 +0300

    forgot sha1

--- homebrew/symbolicate-crash.rb
+++ homebrew/symbolicate-crash.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class SymbolicateCrash < ScriptFileFormula
   url 'https://raw.githubusercontent.com/hamstergene/symbolicate-crash/v0.3/symbolicate-crash'
+  sha1 '1a427f26d3b713aa68d9970bf257f2af992c4ce1'
   version '0.3'
   homepage 'https://github.com/hamstergene/symbolicate-crash'
 end
