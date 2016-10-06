3c4089152b74f8e292960a3eb7697ebba34caa57
Evgeny Khomyakov <ehomyakov@gmail.com>
2016-10-06 15:47:31 +0300

    rm unused

--- homebrew/symbolicate-crash.rb
+++
@@ -1,9 +0,0 @@
-require 'formula'
-
-class SymbolicateCrash < ScriptFileFormula
-  url 'https://raw.githubusercontent.com/hamstergene/symbolicate-crash/v0.6.1/symbolicate-crash'
-  sha1 'ee0255522d1f01545981aea7ac36925e81bbf215'
-  version '0.6.1'
-  homepage 'https://github.com/hamstergene/symbolicate-crash'
-end
-
